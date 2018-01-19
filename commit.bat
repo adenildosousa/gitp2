6f7545d090b82a1f065d370110d3a79c8940cd56
Adenildo Sousa (6052) <asousa@luxfacta.com.br>
2018-01-19 11:22:12 -0200

    gffgsjdfjaksgfsa

--- commit.bat
+++ commit.bat
@@ -1,6 +1,4 @@
 git checkout readme.txt
 git add .
 git commit 
-git push 
-Username for 'https://github.com': adenildosousa
-Password for 'https://USERNAME@github.com': Imperial1.
+git push Username for 'https://github.com': adenildosousa Password for 'https://USERNAME@github.com': Imperial1.
